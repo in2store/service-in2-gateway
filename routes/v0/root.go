7ef6f48fe9073af076645f5c42c8d1fff0a14086
liyiwen <liyiwen@huoyunren.com>
2019-04-20 16:46:29 +0800

    create book interface

--- routes/v0/root.go
+++ routes/v0/root.go
@@ -1,6 +1,7 @@
 package v0
 
 import (
+	"github.com/in2store/service-in2-gateway/routes/v0/books"
 	"github.com/in2store/service-in2-gateway/routes/v0/repos"
 	"github.com/johnnyeven/libtools/courier"
 )
@@ -9,6 +10,7 @@ var Router = courier.NewRouter(V0Group{})
 
 func init() {
 	Router.Register(repos.Router)
+	Router.Register(books.Router)
 }
 
 type V0Group struct {
