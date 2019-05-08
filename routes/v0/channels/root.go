cec7e844173080221973bc3a54756c1430a716ff
liyiwen <liyiwen@huoyunren.com>
2019-05-08 15:52:40 +0800

    get auth url interface

--- routes/v0/channels/root.go
+++ routes/v0/channels/root.go
@@ -1,11 +1,10 @@
 package channels
 
 import (
-	"github.com/in2store/service-in2-gateway/routes/middleware"
 	"github.com/johnnyeven/libtools/courier"
 )
 
-var Router = courier.NewRouter(middleware.MiddlewareAuth, ChannelsGroup{})
+var Router = courier.NewRouter(ChannelsGroup{})
 
 type ChannelsGroup struct {
 	courier.EmptyOperator
